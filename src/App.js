614ab364f2c0d55bfa41b318d83f267086f04c12
abin135 <abi.n135@gmail.com>
2024-08-14 20:35:05 -0400

    Revert "Deleted cat graphic component and cat png"
    This reverts commit a87ddccf9f00e06b610c5a30b124f55b7f2c5099.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Button from './components/Button';
 import FormInput from './components/FormInput';
+import Graphic from './components/Picture';
 import TodoList from './components/TodoList';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   <div className="App"> 
     <Button />
     <FormInput />
+    <Graphic />
     <TodoList />
   </div> 
   ); 
